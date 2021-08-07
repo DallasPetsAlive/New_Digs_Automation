f4d41e4c52727f32ac049aba44b873a41b7a2402
Katie Patterson <kpatterson67@gmail.com>
2021-08-06 21:27:23 -0500

    Update pets made available date

---
+++ new_digs_automation/new_digs_automation.py
@@ -0,0 +1,134 @@
+import json
+import logging
+import requests
+
+from .config import api_key, base
+from datetime import date
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+possible_pet_statuses = [
+    "Accepted, Not Yet Published",
+    "Published - Available for Adoption",
+    "Adoption Pending",
+    "Adopted",
+    "Removed from Program"
+]
+base_url = "https://api.airtable.com/v0/" + base
+today = date.today()
+headers = {"Authorization": "Bearer " + api_key}
+
+
+def lambda_handler(event, context):
+    url = base_url + "/Pets"
+
+    response = requests.get(url, headers=headers)
+    logger.info(json.dumps(json.loads(response.text)))
+    if(response.status_code != requests.codes.ok):
+        logger.error("Airtable response: ")
+        logger.error(response)
+        logger.error("URL: " + url)
+        logger.error("Headers: " + str(headers))
+        return
+
+    airtable_response = response.json()
+
+    # first get pets that are available but don't have an available date
+    available_pets_to_udpate = get_available_pets_to_update(
+        airtable_response["records"]
+    )
+    if not update_available_pets(available_pets_to_udpate):
+        logger.error("Updating pets failed.")
+
+
+def get_available_pets_to_update(pets):
+    pets_to_update = []
+    for pet in pets:
+        pet_fields = pet["fields"]
+
+        # make sure there's no funny business
+        if (
+            "Status" in pet_fields
+            and pet_fields["Status"] not in possible_pet_statuses
+            and len(pet_fields["Status"]) > 0
+        ):
+            error_status = pet_fields["Status"]
+            id = pet["id"]
+            logger.warning(f"Unknown pet status: {error_status} id: {id}")
+            continue
+        if (
+            "Status" not in pet_fields
+            or pet_fields["Status"] == ""
+        ):
+            id = pet["id"]
+            logger.warning(f"Empty/missing pet status id: {id}")
+            continue
+
+        # check if pet is available and available date has not been set
+        if (
+            "Status" in pet_fields
+            and pet_fields["Status"] in [
+                "Published - Available for Adoption",
+                "Adoption Pending",
+                "Adopted",
+                "Removed from Program"
+            ]
+            and (
+                "Made Available for Adoption Date" not in pet_fields
+                or not pet_fields["Made Available for Adoption Date"]
+            )
+        ):
+            pets_to_update.append(pet["id"])
+    return pets_to_update
+
+
+def update_available_pets(pet_ids):
+    update_records = []
+    for id in pet_ids:
+        record = {
+            "id": id,
+            "fields": {
+                "Made Available for Adoption Date": today,
+            }
+        }
+        update_records.append(record)
+
+    if len(update_records) > 0:
+        payload = {
+            "records": update_records
+        }
+        payload = json.dumps(payload, indent=4, default=str)
+        logger.info(payload)
+        url = base_url + "/Pets"
+        patch_headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_key
+        }
+
+        response = requests.patch(url, headers=patch_headers, data=payload)
+        logger.info(response.text)
+        if(response.status_code != requests.codes.ok):
+            logger.error("Patch failed.")
+            logger.error(response.content)
+            return False
+
+        airtable_response = response.json()
+        records = airtable_response["records"]
+        if len(records) != len(update_records):
+            logger.error("Patch returned the wrong number of records.")
+            logger.error(response.content)
+            return False
+        for record in records:
+            if (
+                record["fields"]["Made Available for Adoption Date"]
+                != str(today)
+            ):
+                logger.error("Patch returned the wrong date.")
+                logger.error(response.content)
+                return False
+        return True
+
+
+# logging.basicConfig(filename="log.log", level=logging.DEBUG)
+# update_available_pets(["recOkHgRR68MnYz2k"])
---
+++ tests/test_new_digs.py
@@ -0,0 +1,163 @@
+import logging
+from datetime import date
+from new_digs_automation.config import base
+from new_digs_automation.new_digs_automation import (
+    get_available_pets_to_update,
+    update_available_pets,
+)
+
+logging.basicConfig(filename="log.log", level=logging.DEBUG)
+today = date.today()
+
+
+def test_get_pets_to_set_available_date():
+    test_pets = [
+        {
+            "id": "1",
+            "fields": {
+                "Pet Name": "Test Doggo",
+                "Status": "Published - Available for Adoption",
+            },
+        },
+        {
+            "id": "2",
+            "fields": {
+                "Pet Name": "Test Cat",
+                "Made Available for Adoption Date": "some date",
+                "Status": "Adoption Pending",
+            },
+        },
+        {
+            "id": "3",
+            "fields": {
+                "Pet Name": "Test Birdie",
+                "Made Available for Adoption Date": "",
+                "Status": "Adopted",
+            },
+        },
+        {
+            "id": "4",
+            "fields": {
+                "Pet Name": "Spot",
+                "Made Available for Adoption Date": "",
+                "Status": "Accepted, Not Yet Published",
+            },
+        },
+    ]
+    pet_ids_to_update = get_available_pets_to_update(test_pets)
+    assert pet_ids_to_update == ["1", "3"]
+
+
+def test_check_for_invalid_status(caplog):
+    test_pets = [
+        {
+            "id": "1",
+            "fields": {
+                "Pet Name": "Test Doggo",
+                "Status": "Something Random",
+            },
+        },
+    ]
+    get_available_pets_to_update(test_pets)
+    assert "Unknown pet status: Something Random id: 1" in caplog.text
+
+
+def test_check_for_empty_status(caplog):
+    test_pets = [
+        {
+            "id": "1",
+            "fields": {
+                "Pet Name": "Test Doggo",
+                "Status": "",
+            },
+        },
+    ]
+    get_available_pets_to_update(test_pets)
+    assert "Empty/missing pet status id: 1" in caplog.text
+
+
+def test_check_for_missing_status(caplog):
+    test_pets = [
+        {
+            "id": "1",
+            "fields": {
+                "Pet Name": "Test Doggo",
+            },
+        },
+    ]
+    get_available_pets_to_update(test_pets)
+    assert "Empty/missing pet status id: 1" in caplog.text
+
+
+def test_request_pet_available_update(requests_mock):
+    input = ["1"]
+
+    url = "https://api.airtable.com/v0/" + base + "/Pets"
+    successful_output = {
+        "records": [
+            {
+                "id": "1",
+                "fields": {
+                    "Made Available for Adoption Date": str(today)
+                }
+            }
+        ]
+    }
+
+    requests_mock.patch(url, json=successful_output)
+
+    assert update_available_pets(input)
+
+
+def test_request_pet_available_update_bad_response(requests_mock, caplog):
+    input = ["1"]
+
+    url = "https://api.airtable.com/v0/" + base + "/Pets"
+    error_output = {
+        "status_code": 412
+    }
+
+    requests_mock.patch(
+        url,
+        [
+            error_output
+        ]
+    )
+
+    assert not update_available_pets(input)
+    assert "Patch failed." in caplog.text
+
+
+def test_request_pet_available_update_wrong_length(requests_mock, caplog):
+    input = ["1"]
+
+    url = "https://api.airtable.com/v0/" + base + "/Pets"
+    error_output = {
+        "records": []
+    }
+
+    requests_mock.patch(url, json=error_output)
+
+    assert not update_available_pets(input)
+    assert "Patch returned the wrong number of records." in caplog.text
+
+
+def test_request_pet_available_update_wrong_date(requests_mock, caplog):
+    input = ["1"]
+
+    url = "https://api.airtable.com/v0/" + base + "/Pets"
+    error_output = {
+        "records": [
+            {
+                "id": "1",
+                "fields": {
+                    "Made Available for Adoption Date": "2020-12-12"
+                }
+            }
+        ]
+    }
+
+    requests_mock.patch(url, json=error_output)
+
+    assert not update_available_pets(input)
+    assert "Patch returned the wrong date." in caplog.text
