8d812e7fbabd928e66559ec9fdc6ad1880a3cd45
Katie Patterson <kpatterson67@gmail.com>
2021-08-18 23:17:39 -0500

    create contract links

--- new_digs_automation/automation.py
+++ new_digs_automation/automation.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import requests
+import urllib.parse
 
 from .config import api_key, base
 from .google_sheets import google_sheets_synchronization
@@ -26,17 +27,17 @@ def automations():
     response = requests.get(url, headers=headers)
     logger.info(json.dumps(json.loads(response.text)))
     if(response.status_code != requests.codes.ok):
-        logger.error("Airtable response: ")
+        logger.error("Airtable response Pets: ")
         logger.error(response)
         logger.error("URL: " + url)
         logger.error("Headers: " + str(headers))
         return
 
-    airtable_response = response.json()
+    airtable_pet_response = response.json()
 
     # first get pets that are available but don't have an available date
     available_pets_to_update = get_available_pets_to_update(
-        airtable_response["records"]
+        airtable_pet_response["records"]
     )
     available_pets_updated = 0
     if available_pets_to_update:
@@ -46,10 +47,41 @@ def automations():
         if not available_pets_updated:
             logger.error("Updating pets failed.")
 
+    url = base_url + "/Adoption%20Applicants"
+    response = requests.get(url, headers=headers)
+    logger.info(json.dumps(json.loads(response.text)))
+    if(response.status_code != requests.codes.ok):
+        logger.error("Airtable response Adopt apps: ")
+        logger.error(response)
+        logger.error("URL: " + url)
+        logger.error("Headers: " + str(headers))
+        return
+
+    airtable_adopt_response = response.json()
+
+    url = base_url + "/Original%20Owners"
+    response = requests.get(url, headers=headers)
+    logger.info(json.dumps(json.loads(response.text)))
+    if(response.status_code != requests.codes.ok):
+        logger.error("Airtable response Adopt apps: ")
+        logger.error(response)
+        logger.error("URL: " + url)
+        logger.error("Headers: " + str(headers))
+        return
+
+    airtable_owners_response = response.json()
+
+    contracts_added = add_adoption_contracts(
+        airtable_adopt_response["records"],
+        airtable_pet_response["records"],
+        airtable_owners_response["records"],
+    )
+
     sheets_rows = google_sheets_synchronization()
 
     return {
         "available_pets_updated": available_pets_updated,
+        "adoption_contracts_added": contracts_added,
         "google_sheets_rows_written": sheets_rows,
     }
 
@@ -141,6 +173,152 @@ def update_available_pets(pet_ids):
                 logger.error(response.content)
                 return False
     return len(update_records)
+
+
+def add_adoption_contracts(records, pets, owners):
+    update_records = []
+    for app in records:
+        app_fields = app["fields"]
+        if (
+            "Contract Link" not in app_fields or
+            (
+                "Contract Link" in app_fields
+                and not app_fields["Contract Link"]
+            )
+        ):
+            pet_name = None
+            pet_id = None
+            current_owner_name = None
+            current_owner_id = None
+            if (
+                "Applied For" in app_fields
+                and app_fields["Applied For"]
+            ):
+                pet_record_id = app_fields["Applied For"][0]
+                for pet in pets:
+                    if pet_record_id == pet["id"]:
+                        pet_fields = pet["fields"]
+                        if (
+                            "Pet Name" in pet_fields
+                            and pet_fields["Pet Name"]
+                        ):
+                            pet_name = pet_fields["Pet Name"]
+                        if (
+                            "Pet ID - do not edit" in pet_fields
+                            and pet_fields["Pet ID - do not edit"]
+                        ):
+                            pet_id = pet_fields["Pet ID - do not edit"]
+                        if (
+                            "Original Owner" in pet_fields
+                            and pet_fields["Original Owner"]
+                        ):
+                            current_owner_id = pet_fields["Original Owner"][0]
+                        break
+
+                for owner in owners:
+                    if current_owner_id == owner["id"]:
+                        owner_fields = owner["fields"]
+                        if (
+                            "Name" in owner_fields
+                            and owner_fields["Name"]
+                        ):
+                            current_owner_name = owner_fields["Name"]
+
+            contract_link = get_adoption_app_link(
+                app,
+                pet_name,
+                pet_id,
+                current_owner_name
+            )
+            record = {
+                "id": app["id"],
+                "fields": {
+                    "Contract Link": contract_link,
+                }
+            }
+            update_records.append(record)
+
+    if len(update_records) > 0:
+        payload = {
+            "records": update_records
+        }
+        payload = json.dumps(payload, indent=4, default=str)
+        logger.info(payload)
+        url = base_url + "/Adoption%20Applicants"
+        patch_headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_key
+        }
+
+        response = requests.patch(url, headers=patch_headers, data=payload)
+        logger.info(response.text)
+        if(response.status_code != requests.codes.ok):
+            logger.error("Patch failed.")
+            logger.error(response.content)
+            return False
+
+        airtable_response = response.json()
+        records = airtable_response["records"]
+        if len(records) != len(update_records):
+            logger.error("Patch returned the wrong number of records.")
+            logger.error(response.content)
+            return False
+
+    return len(update_records)
+
+
+def get_adoption_app_link(app, pet_name, pet_id, owner_name):
+    link = "https://dallaspetsalive.org/new-digs-canine-adoption-contract/?"
+    params = {}
+    if pet_name:
+        params["petName"] = pet_name
+    if pet_id:
+        params["petId"] = pet_id
+    if owner_name:
+        space = owner_name.find(" ")
+        owner_first_name = owner_name[:space]
+        owner_last_name = owner_name[space:]
+        params["input6[firstname-3]"] = owner_first_name
+        params["input6[lastname-3]"] = owner_last_name
+
+    app_fields = app["fields"]
+    if (
+        "Name" in app_fields
+        and app_fields["Name"]
+    ):
+        app_name = app_fields["Name"]
+        space = app_name.find(" ")
+        app_first_name = app_name[:space]
+        app_last_name = app_name[space:]
+        params["input6[firstname-4]"] = app_first_name
+        params["input6[lastname-4]"] = app_last_name
+
+    link += urllib.parse.urlencode(params)
+
+    linkRequest = {
+        "destination": link,
+        "domain": {
+            "id": "5e50a8a5d040405c85d8957f23154fc7"
+        },
+    }
+
+    requestHeaders = {
+        "Content-type": "application/json",
+        "apikey": "b92410b92f214eccb8dec17b66dcaadb",
+    }
+
+    r = requests.post(
+        "https://api.rebrandly.com/v1/links",
+        data=json.dumps(linkRequest),
+        headers=requestHeaders
+    )
+
+    if (r.status_code == requests.codes.ok):
+        link = r.json()
+        print(link)
+        print("Long URL was %s, short URL is %s" % (link["destination"], link["shortUrl"]))
+        return link["shortUrl"]
+    return None
 
 
 # logging.basicConfig(filename="log.log", level=logging.DEBUG)
